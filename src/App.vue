6193b81d9c092bf46c9c6ab000e88ea0befed47d
Daniel Ahn <et3rnal.d@gmail.com>
2022-09-13 14:52:33 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import TheHeader from '@/components/TheHeader.vue'
+import EntryEditor from './components/EntryEditor.vue'
+import EntryCard from '@/components/EntryCard.vue'
+import { reactive } from 'vue'
+import type User from './types/User'
+
+
+const user: User = reactive({
+  id: 1,
+  username: 'et3rnal_D',
+  settings: [],
+})
+</script>
+
+<template>
+  <main class="container m-auto p-10">
+    <TheHeader />
+    <EntryEditor />
+    <ul>
+      <li>
+        <EntryCard />
+      </li>
+    </ul>
+  </main>
+</template>
